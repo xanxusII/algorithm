c78eb17973061ca7eba90b38133d0e70e07141f7
xanxus <xanxusmaxwell@yeah.net>
2019-07-03 18:07:58 +0800

    冒泡排序算法的实现

---
+++ src/sort/bubblesort/BubbleSort.java
@@ -0,0 +1,260 @@
+/**
+ * 
+ *
+ * =========================================================================================================================================================
+ * ||                                                                       BQB    BBs                                                                    || 
+ * ||                                                                     BBQBQB  QBQBQ                                                                   || 
+ * ||                                                                      QBBBBBQBQBB                                                                    || 
+ * ||                                 BBQBQ                                  BQBQBQB                                  QBBBQ                               || 
+ * ||                                QBQBBBQ                                  BQBBB                                  BBBBBBB                              || 
+ * ||                                  BQBBB                                  BBQBB                                  BBBBBB                               || 
+ * ||                                  QBQBB                                  BQBBB                                  BBBBB   B                            || 
+ * ||                            BBBQBBBQBBBQB                                BBQBQ                                BQBQBQBBBQBBB                          || 
+ * ||                            BBQBBBBBBBQBQBB                              BQBBB                              BBBBBBQBQBQBQBQ                          || 
+ * ||                             QBBB   BBBQBQBBBQ                           BBBBB                            BBBQBBBBB    BBQ                           || 
+ * ||                                      BBBBQBBBQB                                                       BBBBBBBQBQ                                    || 
+ * ||                                         QBQBQBBBB                     QBiBBBQrQ                     QBQBQBQBQB                                      || 
+ * ||                                           BBBQBQBBBQ              BQBBBQBQBQBBBBBQBQ              BBBBBBBBB                                         || 
+ * ||                                                     BB         BQBQBQBBBBBQBQBBBQBBBBB         BQ                                                   || 
+ * ||                                             BQBQ       B     BBBBBBQBQBQBBBBBQBBBBBBBBBQ     Q       gBBBg                                          || 
+ * ||                                      BQBBBBBBBQBQBBBQ      BQBBBQBBBBBQBBBBBQBQBBBQBBBBBBB      BBBBBBBBBBBBBBBB                                    || 
+ * ||                                   BBBQBBBBBBBBBQBBBQBBBQ  BQBBBQBBBBBBBBBQBBBBBQBQBBBBBQBBB   BQBQBBBBBQBBBBBQBBBBB                                 || 
+ * ||                                 BBQBQBBBBBQBBBBBQBBBQBB  BQBBBBBBBBBQBQBBBQBBBBBQBBBQBQBQBQB  QBQBBBQBBBQBBBQBBBBBQBB                               || 
+ * ||                                BBBBQBBBBBBBBBQBBBQBBBQB  BBQBBBBBQBQBBBQBBBBBBBBBBBQBQBBBBBBB  QBBBQBQBBBBBBBBBBBQBBBQ                              || 
+ * ||                               BBQBQBQBBBBBQBQBBBQBQBQB  BBBBBBQBBBQBQBQBBBQBQBBBQBBBQBBBBBBBB  BQBQBQBBBBBQBQBQBQBBBQBQB                            || 
+ * ||                              QBBBQBBBQBBBBBBBQBQBBBBBB  BBBB   BBQBQBQBQBBBBBBBBBBBBBQB  QBQB  QBBBQBBBBBQBBBBBBBQBBBBBBB                           || 
+ * ||        BQB                  QBQBBBQBBBBBBBQBBBQBQBBBBB  QBQB    QBBBBBQB  B  BBBQBQBQ    QQBQB  QBBBBBBBBBBBBBBBBBQBQBBBQ                  BQBB     || 
+ * ||      QBBBBB                 BQBQBQBBBBBBBBBQ   BBBBQBB  BQBB       BBBBQ     BBBBQ        BBBB  BQBBBBB  QBBBBBQBQBQBBBQBQ                BBBBBQ    || 
+ * ||       BBQBBBQBBBBBBBQBBBBB BBBBBQBQBBBBBQBQ     QBBBBB  QBBB              Q              BBBBQ QQBQBQB    BBQBQBBBQBQBBBQB  BBBBQBQBBBQBBBBBBBQ     || 
+ * ||          BBQBQBBBQBBBQBBB  BBQBBBBBBBQBQBQ      BBBQBB  BQBQB             B             MQBBB  BBBBQBQ     7BQBBBBBQBQBBBB  BBBQBBBBBBBQBQBQ:       || 
+ * ||         QBQBBBBBBBQBQBBBQ  BBBQBQBBBBBBB       BBBQBQBB  BBBQBQ         BBQBB         QBBBQB   BBBQBBB       BQBBBBBBBBBBB  BBQBBBBBBBQBBBBBB       || 
+ * ||       BQBBB                QBQBQBBBQBQ        BBBBBBBQB   BBBBBQBBBBBQBB     BBBBQBBBBBBBBB   BBBQBBBBBQ       QBBBBBQBQBQ                BBBQBB    || 
+ * ||       QBQB                 BB  B  BB       BQBBBQBBBQBQB    BBQBBBBBBBBB     BQBQBBBBBQBB    BBBBBQBBBBBQB       BQB    QB                 BBBQ     || 
+ * ||                                         BQBBBQBQBQBBBBBQB      QBQBBBQBBBB  BBBBBBBBBQ      BQBBBQBBBBBQBBBQB                                       || 
+ * ||                                 B    BQBQBBBQBQBBBBBBB     B     BQBQ QBBBBBBBQ QBBB     B     BQBBBBBBBQBBBQBQB    B                               || 
+ * ||                                 B BBBQBBBQBBBBBBBBBg     BBBBB   BBQ  BBBB  BBB  QBQr  BBQBBB     BBBQBQBBBBBQBBBBB B                               || 
+ * ||                               BB    QBQBQBBBBBQBQ     BBBBBBQB  BBQB  BBBB  BBBB  BBQB  QBQBBBQ      BBBQBQBBBQBQ    BB                             || 
+ * ||                                     BQBBBQBQBBBBB   BBBBBBBQB        BBBBQ  BQBQ        BBBQBBBBBB   QBBBQBQBQBBB                                   || 
+ * ||                                   QBBBBBQBQBB  BQB  QBBBQBQ                               BBBBQBB  BQBB  BBBBBQBBBB                                 || 
+ * ||                                    Q  BB  BQB  BB    QBQB                                    QBQB   BB  BBB  QBB Q                                  || 
+ * ||                                   B  1QB  BBQ      BBB                  BBBBQ                  BBB      QBQ  BQB  B                                 || 
+ * ||                                   B  S           BBB                    BBBQB                    QBQj             B                                 || 
+ * ||                                              BQBBB                      QBBBQ                      BQBBB                                            || 
+ * ||                                       BBBQBBBQB                         BQBBB                         BQBBBBBBB                                     || 
+ * ||                              BB    BBBQBBBBBQ                           BBQBB                           BBBBBBQBQB     B                            || 
+ * ||                            QBBBBBQBQBBBBBQB                             BQBBB                             BQBQBBBQBBBQBBBQ                          || 
+ * ||                            BQBBBQBBBBBBB                                BBQBQ                                BQBBBQBBBQBBB                          || 
+ * ||                             jQB  BBBBB                                  BBBBB                                  BBQBB  BBB                           || 
+ * ||                                  BQBB                                   QBBBB                                   QBBB                                || 
+ * ||                                BBQBQBB                                  BBBBB                                   BQBBBB                              || 
+ * ||                                 BBBBB                                   QBQBBB                                  BBQBB                               || 
+ * ||                                                                      BBBBQBQBQBQ                                                                    || 
+ * ||                                                                     BBQBQB BBBQBB                                                                   || 
+ * ||                                                                      QBQB   BQBQ                                                                    || 
+ * =========================================================================================================================================================
+ * =========================================================================================================================================================	
+ *                                                                         人的梦想是不会结束的！
+ *  @author: xanxus
+ *  
+ *  @date: 2019年7月3日 下午1:10:42
+ *  
+ *  @Description: TODO
+ *
+ *                                                   
+ * =========================================================================================================================================================  
+ *
+ *
+ */
+package sort.bubblesort;
+
+import java.security.Timestamp;
+import java.time.Instant;
+import java.util.Arrays;
+
+import sort.base.SortTestable;
+import sort.util.CommonUtils;
+
+/**
+ *
+ * @author: xanxus
+ *
+ * @date: 2019年7月3日 下午1:10:42
+ * 
+ * @Description: 实现冒泡排序
+ *
+ *
+ */
+public class BubbleSort {
+
+	/** 
+	 * @功能描述:	冒泡排序算法.核心思想：依次相邻两个元素比较、排序。
+	 *
+	 * @参数说明:	@param a:待排序数组;
+	 *          
+	 * @作者:		xanxus
+	 *
+	 * @创建时间:	2019年7月3日 
+	 */  
+	public static void sort(int[] a){
+		
+		for(int i = 1; i < a.length; i++){
+			for(int j = 0; j < a.length - i; j++){
+				if(a[j] > a[j + 1])CommonUtils.swap(a, j, j + 1);
+			}
+		}
+	}
+	
+	/** 
+	 * @功能描述:	基于基础sort算法的改进。sort算法中有很多重复多余的比较过程，内部循环可以直接
+	 * 			从上一次数组发生改变的前一个位置开始（如果小于0，则从0开始）
+	 *
+	 * @参数说明:	@param a
+	 *          
+	 * @作者:		xanxus
+	 *
+	 * @创建时间:	2019年7月3日 
+	 */  
+	public static void sort1(int[] a){
+		int loc = 0;
+		boolean flag = false;
+		for(int i = 1; i < a.length; i++){
+			flag = false;
+			for(int j = loc; j < a.length - i; j++){
+				if(a[j] > a[j + 1]){
+					CommonUtils.swap(a, j, j + 1);
+					if(!flag){
+						flag = true;
+						//记录第一次交换元素的位置，下一次循环可以直接以该位置为起点。
+						loc = j - 1;
+						if(loc < 0)loc = 0;
+					}
+				}
+			}
+			if(!flag)break;
+		}
+	}
+	
+	/** 
+	 * @功能描述: 同sort1的优化，既然sort1可以优化内部循环的起始位置，同理sort2优化内部循环的结束位置。不过测试下来效果并不明显。
+	 *
+	 * @参数说明:	@param a
+	 *          
+	 * @返回值:	void
+	 *
+	 * @作者:		xanxus
+	 *
+	 * @创建时间:	2019年7月3日 
+	 */  
+	public static void sort2(int[] a){
+		int loc = 0;
+		boolean flag = false;
+		int h = a.length - 1;
+		int end = h;
+		for(int i = 1; i < a.length; i++){
+			flag = false;
+			for(int j = loc; j < h; j++){
+				if(a[j] > a[j + 1]){
+					CommonUtils.swap(a, j, j + 1);
+					end = j;
+					flag = true;
+				}
+			}
+			//记录最后一次交换发生的位置。
+			h = end;
+			if(!flag)break;
+		}
+	}
+	
+	/** 
+	 * @功能描述:	结合sort1和sort2， 同时优化内部循环的起始和结束位置。 效果相对sort1较差。
+	 *
+	 * @参数说明:	@param a
+	 *          
+	 * @返回值:	void
+	 *
+	 * @作者:		xanxus
+	 *
+	 * @创建时间:	2019年7月3日 
+	 */  
+	public static void sort3(int[] a){
+		int l = 0;
+		int h = a.length - 1;
+		int end = h;
+		boolean flag = false;
+		for(int i = 1; i < a.length; i++){
+			flag = false;
+			for(int j = l; j < a.length - i; j++){
+				if(a[j] > a[j + 1]){
+					CommonUtils.swap(a, j, j + 1);
+					end = j;
+					if(!flag){
+						flag = true;
+						//记录第一次交换元素的位置，下一次循环可以直接以该位置为起点。
+						l = j - 1;
+						if(l < 0)l = 0;
+					}
+				}
+			}
+			h = end;
+			if(!flag)break;
+		}
+	}
+	
+	public static void main(String[] args){
+//		初始化待排序数组
+		int[] sort_data_0_100 = CommonUtils.createIntArrays(50000, 100);
+		int[] sort_data_0_100_1 = CommonUtils.copyArrays(sort_data_0_100);
+		int[] sort_data_0_100_2 = CommonUtils.copyArrays(sort_data_0_100);
+		int[] sort_data_0_100_3 = CommonUtils.copyArrays(sort_data_0_100);
+		
+		SortTestable testSort = (int[] a) -> sort(a);
+		SortTestable testSort1 = (int[] a) -> sort1(a);
+		SortTestable testSort2 = (int[] a) -> sort2(a);
+		SortTestable testSort3 = (int[] a) -> sort3(a);
+//		运行结果如下：
+//		=============================================
+//		sort算法排序是否正确: 排序正确.
+//		sort算法排序用时：3854
+//		sort1算法排序是否正确: 排序正确.
+//		sort1算法排序用时：3499
+//		sort2算法排序是否正确: 排序正确.
+//		sort2算法排序用时：3879
+//		sort3算法排序是否正确: 排序正确.
+//		sort3算法排序用时：3760
+//      =============================================
+//		看来这种通过内部循环的优化获得的提升有限。
+//		=============================================
+		doTestSort("sort", sort_data_0_100, testSort);
+		doTestSort("sort1", sort_data_0_100_1, testSort1);
+		doTestSort("sort2", sort_data_0_100_2, testSort2);
+		doTestSort("sort3", sort_data_0_100_3, testSort3);
+
+	}
+	
+
+	
+	public static void doTestSort(String name , int[] a,SortTestable testSort){
+//		System.out.println(name + "算法待排序数组: " + Arrays.toString(a));
+		long startTime1 = System.currentTimeMillis();
+		testSort.test(a);
+		long endTime1 = System.currentTimeMillis();
+		int isSorted1 = CommonUtils.validateResults(a);
+	
+		System.out.println(name + "算法排序是否正确: " + (isSorted1 > -1 ? "排序错误." + isSorted1 : "排序正确." ));
+//		System.out.println(name + "算法排序结果: " + Arrays.toString(sort_data_0_100_1));
+		System.out.println(name + "算法排序用时："  + (endTime1 - startTime1));
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/sort/util/CommonUtils.java
@@ -0,0 +1,149 @@
+/**
+ * 
+ *
+ * =========================================================================================================================================================
+ * ||                                                                       BQB    BBs                                                                    || 
+ * ||                                                                     BBQBQB  QBQBQ                                                                   || 
+ * ||                                                                      QBBBBBQBQBB                                                                    || 
+ * ||                                 BBQBQ                                  BQBQBQB                                  QBBBQ                               || 
+ * ||                                QBQBBBQ                                  BQBBB                                  BBBBBBB                              || 
+ * ||                                  BQBBB                                  BBQBB                                  BBBBBB                               || 
+ * ||                                  QBQBB                                  BQBBB                                  BBBBB   B                            || 
+ * ||                            BBBQBBBQBBBQB                                BBQBQ                                BQBQBQBBBQBBB                          || 
+ * ||                            BBQBBBBBBBQBQBB                              BQBBB                              BBBBBBQBQBQBQBQ                          || 
+ * ||                             QBBB   BBBQBQBBBQ                           BBBBB                            BBBQBBBBB    BBQ                           || 
+ * ||                                      BBBBQBBBQB                                                       BBBBBBBQBQ                                    || 
+ * ||                                         QBQBQBBBB                     QBiBBBQrQ                     QBQBQBQBQB                                      || 
+ * ||                                           BBBQBQBBBQ              BQBBBQBQBQBBBBBQBQ              BBBBBBBBB                                         || 
+ * ||                                                     BB         BQBQBQBBBBBQBQBBBQBBBBB         BQ                                                   || 
+ * ||                                             BQBQ       B     BBBBBBQBQBQBBBBBQBBBBBBBBBQ     Q       gBBBg                                          || 
+ * ||                                      BQBBBBBBBQBQBBBQ      BQBBBQBBBBBQBBBBBQBQBBBQBBBBBBB      BBBBBBBBBBBBBBBB                                    || 
+ * ||                                   BBBQBBBBBBBBBQBBBQBBBQ  BQBBBQBBBBBBBBBQBBBBBQBQBBBBBQBBB   BQBQBBBBBQBBBBBQBBBBB                                 || 
+ * ||                                 BBQBQBBBBBQBBBBBQBBBQBB  BQBBBBBBBBBQBQBBBQBBBBBQBBBQBQBQBQB  QBQBBBQBBBQBBBQBBBBBQBB                               || 
+ * ||                                BBBBQBBBBBBBBBQBBBQBBBQB  BBQBBBBBQBQBBBQBBBBBBBBBBBQBQBBBBBBB  QBBBQBQBBBBBBBBBBBQBBBQ                              || 
+ * ||                               BBQBQBQBBBBBQBQBBBQBQBQB  BBBBBBQBBBQBQBQBBBQBQBBBQBBBQBBBBBBBB  BQBQBQBBBBBQBQBQBQBBBQBQB                            || 
+ * ||                              QBBBQBBBQBBBBBBBQBQBBBBBB  BBBB   BBQBQBQBQBBBBBBBBBBBBBQB  QBQB  QBBBQBBBBBQBBBBBBBQBBBBBBB                           || 
+ * ||        BQB                  QBQBBBQBBBBBBBQBBBQBQBBBBB  QBQB    QBBBBBQB  B  BBBQBQBQ    QQBQB  QBBBBBBBBBBBBBBBBBQBQBBBQ                  BQBB     || 
+ * ||      QBBBBB                 BQBQBQBBBBBBBBBQ   BBBBQBB  BQBB       BBBBQ     BBBBQ        BBBB  BQBBBBB  QBBBBBQBQBQBBBQBQ                BBBBBQ    || 
+ * ||       BBQBBBQBBBBBBBQBBBBB BBBBBQBQBBBBBQBQ     QBBBBB  QBBB              Q              BBBBQ QQBQBQB    BBQBQBBBQBQBBBQB  BBBBQBQBBBQBBBBBBBQ     || 
+ * ||          BBQBQBBBQBBBQBBB  BBQBBBBBBBQBQBQ      BBBQBB  BQBQB             B             MQBBB  BBBBQBQ     7BQBBBBBQBQBBBB  BBBQBBBBBBBQBQBQ:       || 
+ * ||         QBQBBBBBBBQBQBBBQ  BBBQBQBBBBBBB       BBBQBQBB  BBBQBQ         BBQBB         QBBBQB   BBBQBBB       BQBBBBBBBBBBB  BBQBBBBBBBQBBBBBB       || 
+ * ||       BQBBB                QBQBQBBBQBQ        BBBBBBBQB   BBBBBQBBBBBQBB     BBBBQBBBBBBBBB   BBBQBBBBBQ       QBBBBBQBQBQ                BBBQBB    || 
+ * ||       QBQB                 BB  B  BB       BQBBBQBBBQBQB    BBQBBBBBBBBB     BQBQBBBBBQBB    BBBBBQBBBBBQB       BQB    QB                 BBBQ     || 
+ * ||                                         BQBBBQBQBQBBBBBQB      QBQBBBQBBBB  BBBBBBBBBQ      BQBBBQBBBBBQBBBQB                                       || 
+ * ||                                 B    BQBQBBBQBQBBBBBBB     B     BQBQ QBBBBBBBQ QBBB     B     BQBBBBBBBQBBBQBQB    B                               || 
+ * ||                                 B BBBQBBBQBBBBBBBBBg     BBBBB   BBQ  BBBB  BBB  QBQr  BBQBBB     BBBQBQBBBBBQBBBBB B                               || 
+ * ||                               BB    QBQBQBBBBBQBQ     BBBBBBQB  BBQB  BBBB  BBBB  BBQB  QBQBBBQ      BBBQBQBBBQBQ    BB                             || 
+ * ||                                     BQBBBQBQBBBBB   BBBBBBBQB        BBBBQ  BQBQ        BBBQBBBBBB   QBBBQBQBQBBB                                   || 
+ * ||                                   QBBBBBQBQBB  BQB  QBBBQBQ                               BBBBQBB  BQBB  BBBBBQBBBB                                 || 
+ * ||                                    Q  BB  BQB  BB    QBQB                                    QBQB   BB  BBB  QBB Q                                  || 
+ * ||                                   B  1QB  BBQ      BBB                  BBBBQ                  BBB      QBQ  BQB  B                                 || 
+ * ||                                   B  S           BBB                    BBBQB                    QBQj             B                                 || 
+ * ||                                              BQBBB                      QBBBQ                      BQBBB                                            || 
+ * ||                                       BBBQBBBQB                         BQBBB                         BQBBBBBBB                                     || 
+ * ||                              BB    BBBQBBBBBQ                           BBQBB                           BBBBBBQBQB     B                            || 
+ * ||                            QBBBBBQBQBBBBBQB                             BQBBB                             BQBQBBBQBBBQBBBQ                          || 
+ * ||                            BQBBBQBBBBBBB                                BBQBQ                                BQBBBQBBBQBBB                          || 
+ * ||                             jQB  BBBBB                                  BBBBB                                  BBQBB  BBB                           || 
+ * ||                                  BQBB                                   QBBBB                                   QBBB                                || 
+ * ||                                BBQBQBB                                  BBBBB                                   BQBBBB                              || 
+ * ||                                 BBBBB                                   QBQBBB                                  BBQBB                               || 
+ * ||                                                                      BBBBQBQBQBQ                                                                    || 
+ * ||                                                                     BBQBQB BBBQBB                                                                   || 
+ * ||                                                                      QBQB   BQBQ                                                                    || 
+ * =========================================================================================================================================================
+ * =========================================================================================================================================================	
+ *                                                                         人的梦想是不会结束的！
+ *  @author: xanxus
+ *  
+ *  @date: 2019年7月3日 下午1:37:48
+ *  
+ *  @Description: TODO
+ *
+ *                                                   
+ * =========================================================================================================================================================  
+ *
+ *
+ */
+package sort.util;
+
+import java.util.Random;
+
+/**
+ *
+ * @author: xanxus
+ *
+ * @date: 2019年7月3日 下午1:37:48
+ * 
+ * @Description: 常用排序工具类
+ *
+ *
+ */
+public class CommonUtils {
+	
+
+	/** 
+	 * @功能描述:	验证排序结果是否正确
+	 *
+	 * @参数说明:	@param 待验证数组
+	 *          
+	 * @返回值:	boolean类型: true 数组排序正确; false 排序错误 
+	 *
+	 * @作者:		xanxus
+	 *
+	 * @创建时间:	2019年7月3日 
+	 */  
+	public static int validateResults(int[] a){
+		for(int i = 0; i < a.length - 1; i++)
+			if(a[i] > a[i + 1])return i;
+		return -1;
+	}
+	
+  
+ 
+	/** 
+	 * @功能描述:	创建随机数组(元素在[0, max)之间)
+	 *
+	 * @参数说明:	@param length:	需要创建的数组长度
+	 * 
+	 * @参数说明:	@param max:		数组元素的最大值(不包括该值)
+	 * 
+	 * @返回值:	int[]
+	 *
+	 * @作者:		xanxus
+	 *
+	 * @创建时间:	2019年7月3日 
+	 */  
+	public static int[] createIntArrays(int length, int max){
+		if(max <= 0)throw new IllegalArgumentException("max的值应大于0. " + max);
+		int[] a = new int[length];
+		Random random = new Random(System.currentTimeMillis());
+		for(int i = 0; i < a.length; i++){
+			a[i] = random.nextInt(max);
+		}
+		return a;
+	}
+	
+	/** 
+	 * @功能描述:	交换数组中任意两个元素的值
+	 *
+	 * @参数说明:	@param a
+	 * @参数说明:	@param i
+	 * @参数说明:	@param j
+	 *          
+	 * @作者:		xanxus
+	 *
+	 * @创建时间:	2019年7月3日 
+	 */  
+	public static void swap(int[] a, int i, int j){
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
+	public static int[] copyArrays(int[] src){
+		int[] dest = new int[src.length];
+		System.arraycopy(src, 0, dest, 0, src.length);
+		return dest;
+	}
+}
