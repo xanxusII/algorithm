39d75dbfa088cd0aca9e4bf638c31092bfee0a71
xanxus <xanxusmaxwell@yeah.net>
2019-07-04 20:12:02 +0800

    新增选择排序

---
+++ src/sort/selectionSort/SelectionSort.java
@@ -0,0 +1,143 @@
+/**
+ * 
+ *
+ * =========================================================================================================================================================
+ * ||                                                                       BQB    BBs                                                                    || 
+ * ||                                                                     BBQBQB  QBQBQ                                                                   || 
+ * ||                                                                      QBBBBBQBQBB                                                                    || 
+ * ||                                 BBQBQ                                  BQBQBQB                                  QBBBQ                               || 
+ * ||                                QBQBBBQ                                  BQBBB                                  BBBBBBB                              || 
+ * ||                                  BQBBB                                  BBQBB                                  BBBBBB                               || 
+ * ||                                  QBQBB                                  BQBBB                                  BBBBB   B                            || 
+ * ||                            BBBQBBBQBBBQB                                BBQBQ                                BQBQBQBBBQBBB                          || 
+ * ||                            BBQBBBBBBBQBQBB                              BQBBB                              BBBBBBQBQBQBQBQ                          || 
+ * ||                             QBBB   BBBQBQBBBQ                           BBBBB                            BBBQBBBBB    BBQ                           || 
+ * ||                                      BBBBQBBBQB                                                       BBBBBBBQBQ                                    || 
+ * ||                                         QBQBQBBBB                     QBiBBBQrQ                     QBQBQBQBQB                                      || 
+ * ||                                           BBBQBQBBBQ              BQBBBQBQBQBBBBBQBQ              BBBBBBBBB                                         || 
+ * ||                                                     BB         BQBQBQBBBBBQBQBBBQBBBBB         BQ                                                   || 
+ * ||                                             BQBQ       B     BBBBBBQBQBQBBBBBQBBBBBBBBBQ     Q       gBBBg                                          || 
+ * ||                                      BQBBBBBBBQBQBBBQ      BQBBBQBBBBBQBBBBBQBQBBBQBBBBBBB      BBBBBBBBBBBBBBBB                                    || 
+ * ||                                   BBBQBBBBBBBBBQBBBQBBBQ  BQBBBQBBBBBBBBBQBBBBBQBQBBBBBQBBB   BQBQBBBBBQBBBBBQBBBBB                                 || 
+ * ||                                 BBQBQBBBBBQBBBBBQBBBQBB  BQBBBBBBBBBQBQBBBQBBBBBQBBBQBQBQBQB  QBQBBBQBBBQBBBQBBBBBQBB                               || 
+ * ||                                BBBBQBBBBBBBBBQBBBQBBBQB  BBQBBBBBQBQBBBQBBBBBBBBBBBQBQBBBBBBB  QBBBQBQBBBBBBBBBBBQBBBQ                              || 
+ * ||                               BBQBQBQBBBBBQBQBBBQBQBQB  BBBBBBQBBBQBQBQBBBQBQBBBQBBBQBBBBBBBB  BQBQBQBBBBBQBQBQBQBBBQBQB                            || 
+ * ||                              QBBBQBBBQBBBBBBBQBQBBBBBB  BBBB   BBQBQBQBQBBBBBBBBBBBBBQB  QBQB  QBBBQBBBBBQBBBBBBBQBBBBBBB                           || 
+ * ||        BQB                  QBQBBBQBBBBBBBQBBBQBQBBBBB  QBQB    QBBBBBQB  B  BBBQBQBQ    QQBQB  QBBBBBBBBBBBBBBBBBQBQBBBQ                  BQBB     || 
+ * ||      QBBBBB                 BQBQBQBBBBBBBBBQ   BBBBQBB  BQBB       BBBBQ     BBBBQ        BBBB  BQBBBBB  QBBBBBQBQBQBBBQBQ                BBBBBQ    || 
+ * ||       BBQBBBQBBBBBBBQBBBBB BBBBBQBQBBBBBQBQ     QBBBBB  QBBB              Q              BBBBQ QQBQBQB    BBQBQBBBQBQBBBQB  BBBBQBQBBBQBBBBBBBQ     || 
+ * ||          BBQBQBBBQBBBQBBB  BBQBBBBBBBQBQBQ      BBBQBB  BQBQB             B             MQBBB  BBBBQBQ     7BQBBBBBQBQBBBB  BBBQBBBBBBBQBQBQ:       || 
+ * ||         QBQBBBBBBBQBQBBBQ  BBBQBQBBBBBBB       BBBQBQBB  BBBQBQ         BBQBB         QBBBQB   BBBQBBB       BQBBBBBBBBBBB  BBQBBBBBBBQBBBBBB       || 
+ * ||       BQBBB                QBQBQBBBQBQ        BBBBBBBQB   BBBBBQBBBBBQBB     BBBBQBBBBBBBBB   BBBQBBBBBQ       QBBBBBQBQBQ                BBBQBB    || 
+ * ||       QBQB                 BB  B  BB       BQBBBQBBBQBQB    BBQBBBBBBBBB     BQBQBBBBBQBB    BBBBBQBBBBBQB       BQB    QB                 BBBQ     || 
+ * ||                                         BQBBBQBQBQBBBBBQB      QBQBBBQBBBB  BBBBBBBBBQ      BQBBBQBBBBBQBBBQB                                       || 
+ * ||                                 B    BQBQBBBQBQBBBBBBB     B     BQBQ QBBBBBBBQ QBBB     B     BQBBBBBBBQBBBQBQB    B                               || 
+ * ||                                 B BBBQBBBQBBBBBBBBBg     BBBBB   BBQ  BBBB  BBB  QBQr  BBQBBB     BBBQBQBBBBBQBBBBB B                               || 
+ * ||                               BB    QBQBQBBBBBQBQ     BBBBBBQB  BBQB  BBBB  BBBB  BBQB  QBQBBBQ      BBBQBQBBBQBQ    BB                             || 
+ * ||                                     BQBBBQBQBBBBB   BBBBBBBQB        BBBBQ  BQBQ        BBBQBBBBBB   QBBBQBQBQBBB                                   || 
+ * ||                                   QBBBBBQBQBB  BQB  QBBBQBQ                               BBBBQBB  BQBB  BBBBBQBBBB                                 || 
+ * ||                                    Q  BB  BQB  BB    QBQB                                    QBQB   BB  BBB  QBB Q                                  || 
+ * ||                                   B  1QB  BBQ      BBB                  BBBBQ                  BBB      QBQ  BQB  B                                 || 
+ * ||                                   B  S           BBB                    BBBQB                    QBQj             B                                 || 
+ * ||                                              BQBBB                      QBBBQ                      BQBBB                                            || 
+ * ||                                       BBBQBBBQB                         BQBBB                         BQBBBBBBB                                     || 
+ * ||                              BB    BBBQBBBBBQ                           BBQBB                           BBBBBBQBQB     B                            || 
+ * ||                            QBBBBBQBQBBBBBQB                             BQBBB                             BQBQBBBQBBBQBBBQ                          || 
+ * ||                            BQBBBQBBBBBBB                                BBQBQ                                BQBBBQBBBQBBB                          || 
+ * ||                             jQB  BBBBB                                  BBBBB                                  BBQBB  BBB                           || 
+ * ||                                  BQBB                                   QBBBB                                   QBBB                                || 
+ * ||                                BBQBQBB                                  BBBBB                                   BQBBBB                              || 
+ * ||                                 BBBBB                                   QBQBBB                                  BBQBB                               || 
+ * ||                                                                      BBBBQBQBQBQ                                                                    || 
+ * ||                                                                     BBQBQB BBBQBB                                                                   || 
+ * ||                                                                      QBQB   BQBQ                                                                    || 
+ * =========================================================================================================================================================
+ * =========================================================================================================================================================	
+ *                                                                         人的梦想是不会结束的！
+ *  @作者: xanxus
+ *  
+ *  @创建时间: 2019年7月4日 下午4:47:43
+ *  
+ *  @描述: TODO
+ *
+ *                                                   
+ * =========================================================================================================================================================  
+ *
+ *
+ */
+package sort.selectionSort;
+
+import sort.base.SortTestable;
+import sort.util.CommonUtils;
+
+/**
+ *
+ * @作者: xanxus
+ *
+ * @创建时间: 2019年7月4日 下午4:47:43
+ * 
+ * @功能描述: TODO
+ *
+ * @版本：V1.0 
+ *
+ */
+public class SelectionSort {
+	
+	/** 
+	 * @功能描述:	选择排序
+	 *
+	 * @参数说明:	a
+	 *          
+	 * @返回值:	void
+	 *
+	 * @作者:	xanxus
+	 *
+	 * @创建时间:	2019年7月4日 
+	 */  
+	public static void sort(int[] a){
+		int pos = 0;
+		int swapCount = 0;		//统计swap次数
+		int compareCount = 0;	//统计比较次数
+		for(int i = 0; i < a.length - 1; i++){
+			int min = a[i];
+			pos = i;
+			for(int j = i + 1; j < a.length; j++){
+				if(min > a[j]){
+					min = a[j];
+					pos = j;
+					
+				}
+				compareCount++;
+			}
+			CommonUtils.swap(a, i, pos);
+			swapCount++;
+		}
+		System.out.println("排序完成！排序过程中进行比较:" + compareCount +"次, 交换:" + swapCount+"次.");
+	}
+
+	/** 
+	 * @功能描述:	TODO
+	 *
+	 * @参数说明:	args
+	 *          
+	 * @返回值:	void
+	 *
+	 * @作者:	xanxus
+	 *
+	 * @创建时间:	2019年7月4日 
+	 */
+	public static void main(String[] args) {
+		int[] sort_data_0_100 = CommonUtils.createIntArrays(50000, 100);
+		SortTestable testSort = (int[] a) -> sort(a);
+		CommonUtils.doTestSort("sort", sort_data_0_100, testSort);
+	}
+
+//	sort排序算法开始排序:......
+//	排序完成！排序过程中进行比较:1249975000次, 交换:49999次.
+//	sort算法排序是否正确: 排序正确.
+//	sort算法排序用时：414
+//	=================================================================
+//	可以明显看出相对于冒泡排序，选择排序的交换次数明显较少,比较次数没有明显较少。
+//	=================================================================
+	
+}
